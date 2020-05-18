4ee5fe721e429cf1d6f92f95d01b49dfe592d6f5
MyPC <daflaoui.karim@yahoo.com>
2020-05-18 00:33:52 -0400

    Initial commit

---
+++ lib/screens/Home_Screen.dart
@@ -0,0 +1,123 @@
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:traval_app/widgets/DestinationCarousel.dart';
+import 'package:traval_app/widgets/HotelCarousel.dart';
+
+class HomeScreen extends StatefulWidget{
+  final String title;
+
+  HomeScreen({Key key, this.title}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
+  }
+
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int _selectedIndex = 0;
+  int _currentTab = 0;
+
+  List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.walking,
+    FontAwesomeIcons.biking
+  ];
+
+  //Conctruct the widget of the icons
+  Widget buildIcons(int index){
+    return GestureDetector(
+      onTap: (){
+        setState(() {
+          _selectedIndex = index;
+        });
+        print(_selectedIndex);
+      },
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          color:  _selectedIndex == index ? Theme.of(context).accentColor : Color(0xFFE7EBEE),
+          borderRadius: BorderRadius.circular(30.0)
+        ),
+        child: Icon(
+          _icons[index],
+          size: 25,
+          color: _selectedIndex == index ? Theme.of(context).primaryColor : Color(0xFFB4C1C4),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    // TODO: implement build
+    return Scaffold(
+      body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.symmetric(vertical: 30.0),
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, right: 120.0),
+                child: Text(
+                  'My tourism App',
+                  //'What do you like to find?',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold
+                  )
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: _icons
+                    .asMap()
+                    .entries
+                    .map(
+                      (MapEntry map) => buildIcons(map.key)
+                    ).toList(),
+              ),
+              SizedBox(height: 20.0,),
+              DistinationCarousel(),
+              SizedBox(height: 20.0,),
+              HotelCarousel(),
+                ],
+              )
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _currentTab,
+            onTap: (int value){
+              setState(() {
+                _currentTab = value;
+              });
+            },
+            items:[
+              BottomNavigationBarItem(
+                icon : Icon(Icons.search, size: 30.0),
+                title: SizedBox.shrink()
+              ),
+              BottomNavigationBarItem(
+                  icon : Icon(Icons.local_pizza, size: 30.0),
+                  title: SizedBox.shrink()
+              ),
+              BottomNavigationBarItem(
+                  icon : CircleAvatar(
+                    radius:15.0,
+                    backgroundImage: NetworkImage('http://i.imgur.com/zL4Krbz.jpg'),
+                  ),
+                  title: SizedBox.shrink()
+              )
+            ],
+          )
+        );
+
+
+  }
+
+}
